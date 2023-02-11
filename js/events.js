aa3430d4cb45d836bfe71566aee51398094e3e85
sajim-khan <shossain1427@gmail.com>
2023-02-11 16:27:25 +0600

    added another way of event handler

--- js/events.js
+++ js/events.js
@@ -5,9 +5,16 @@ function makeRed() {
     document.body.style.backgroundColor ='red'
 }
 
+// option 3
 const makeBlueButton = document.getElementById("make-blue");
-        makeBlueButton.onclick = makeBlue;
-        
+        makeBlueButton.onclick = makeBlue;        
 function makeBlue() {
     document.body.style.backgroundColor ='blue'
 }
+
+//option 3 another way
+const makePurpleButton = document.getElementById("make-purple");
+makePurpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor ='purple'
+}
+
