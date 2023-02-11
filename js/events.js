380d80b91a41b8ce859c1e63bb9ff0a6dbb456ed
sajim-khan <shossain1427@gmail.com>
2023-02-11 14:03:28 +0600

    added

--- js/events.js
+++ js/events.js
@@ -1,1 +1,13 @@
 console.log("Welcome to js dom events");
+
+// Option 2
+function makeRed() {
+    document.body.style.backgroundColor ='red'
+}
+
+const makeBlueButton = document.getElementById("make-blue");
+        makeBlueButton.onclick = makeBlue;
+        
+function makeBlue() {
+    document.body.style.backgroundColor ='blue'
+}
